0d715866171d83b13a4ef82e2e29c9d6ed5e96db
adrianhitz <32393038+adrianhitz@users.noreply.github.com>
2018-12-22 19:51:47 +0100

    Add removal of comments and strings to Tokeniser

--- src/ch/fhnw/edu/efalg/token/Tokeniser.java
+++ src/ch/fhnw/edu/efalg/token/Tokeniser.java
@@ -14,9 +14,67 @@ public final class Tokeniser {
     }
 
     public List<Token> tokenise(final String s) {
+        var noComment = removeCommentsAndStrings(s);
+        // TODO Tokenise
         List<Token> tokens = new ArrayList<>();
-        var lines = s.split("\n");
-        // TODO Read lines, filter comments, correctly parse strings
         return tokens;
+    }
+
+    private enum Reading {
+        StringLiteral, LineComment, BlockComment, Other
+    }
+
+    private static String removeCommentsAndStrings(final String s) {
+        // Remove comments and replace string literals
+        // TODO Maybe also replace char literals?
+        var chars = s.toCharArray();
+        var sb = new StringBuilder();
+        var state = Reading.Other;
+        for(int i = 0; i < chars.length; i++) {
+            switch(state) {
+                case StringLiteral:
+                    switch(chars[i]) {
+                        case '\\':
+                            i++;
+                            break;
+                        case '"':
+                            sb.append("StringLiteral");
+                            state = Reading.Other;
+                    }
+                    break;
+                case LineComment:
+                    if(chars[i] == '\n') {
+                        state = Reading.Other;
+                        sb.append(chars[i]);
+                    }
+                    break;
+                case BlockComment:
+                    if(chars[i] == '*' && i + 1 < chars.length && chars[i + 1] == '/') {
+                        state = Reading.Other;
+                        i++;
+                    }
+                    break;
+                default:
+                    switch(chars[i]) {
+                        case '"':
+                            state = Reading.StringLiteral;
+                            break;
+                        case '/':
+                            if(i + 1 < chars.length && chars[i + 1] == '/') {
+                                state = Reading.LineComment;
+                                i++;
+                            } else if(i + 1 < chars.length && chars[i + 1] == '*') {
+                                state = Reading.BlockComment;
+                                i++;
+                            } else {
+                                sb.append(chars[i]);
+                            }
+                            break;
+                        default:
+                            sb.append(chars[i]);
+                    }
+            }
+        }
+        return sb.toString();
     }
 }
