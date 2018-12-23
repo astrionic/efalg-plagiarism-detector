90d9870cda0bd517b045bb78b0e5b620db9500e2
adrianhitz <32393038+adrianhitz@users.noreply.github.com>
2018-12-23 17:22:05 +0100

    Document Token

--- src/ch/fhnw/edu/efalg/token/Token.java
+++ src/ch/fhnw/edu/efalg/token/Token.java
@@ -1,5 +1,8 @@
 package ch.fhnw.edu.efalg.token;
 
+/**
+ * Represents the types of tokens which the {@code Tokeniser} could potentially create.
+ */
 public enum Token {
     Keyword, Identifier, Separator
 }
