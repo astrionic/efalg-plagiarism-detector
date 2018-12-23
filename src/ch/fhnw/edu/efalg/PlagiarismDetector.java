0dc493cff823485c20f7d898e18f9699542c51dd
adrianhitz <32393038+adrianhitz@users.noreply.github.com>
2018-12-23 12:35:25 +0100

    Inverted index calculation

--- src/ch/fhnw/edu/efalg/PlagiarismDetector.java
+++ src/ch/fhnw/edu/efalg/PlagiarismDetector.java
@@ -4,8 +4,8 @@ import ch.fhnw.edu.efalg.token.TokenType;
 import ch.fhnw.edu.efalg.token.Tokeniser;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class PlagiarismDetector {
     private final Tokeniser tokeniser;
@@ -14,6 +14,26 @@ public class PlagiarismDetector {
         final var keywords = Reader.readJavaKeywords();
         final var separators = Reader.readJavaSeparators();
         this.tokeniser = new Tokeniser(keywords, separators);
+    }
+
+    public void calculateSimilarities() throws IOException {
+        final var programs = Reader.readJavaFiles("input");
+        final var tokenLists = tokenise(programs);
+        final List<List<FourGram>> fourGramLists = tokenLists.stream().map(this::toFourGramList).collect(Collectors.toList()); // TODO Array?
+
+        // Inverted index TODO Put into its own method?2
+        final var inv = new HashMap<FourGram, Set<Integer>>();
+        for(int i = 0; i < fourGramLists.size(); i++) {
+            for(var fourGram : fourGramLists.get(i)) {
+                if(!inv.containsKey(fourGram)) {
+                    inv.put(fourGram, new HashSet<>());
+                }
+                inv.get(fourGram).add(i);
+            }
+        }
+        // Calculate similarity matrix
+
+        // TODO Finish calculateSimilarities implementation
     }
 
     private List<List<TokenType>> tokenise(String[] programs) throws IOException {
@@ -38,13 +58,6 @@ public class PlagiarismDetector {
             fourGramLists.add(toFourGramList(tokenList));
         }
         return fourGramLists;
-    }
-
-    public void calculateSimilarities() throws IOException {
-        final var programs = Reader.readJavaFiles("input");
-        final var tokenLists = tokenise(programs);
-        final var fourGramLists = toFourGramLists(tokenLists);
-        // TODO Finish calculateSimilarities implementation
     }
 
 }
